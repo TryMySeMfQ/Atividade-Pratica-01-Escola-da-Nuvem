165bd06c856d89aa92402469073f2ca981697bff
Julia Tito <94010304+TryMySeMfQ@users.noreply.github.com>
2025-10-28 19:28:47 -0300

    Add files via upload

---
+++ atividade01.py
@@ -0,0 +1,44 @@
+# ----- Exercício 1 -----
+# Programa de Saudação
+print("Hello, world!")
+
+# ----- Exercício 2 -----
+# Calculadora de Soma
+numero1 = 12
+numero2 = 14
+soma = numero1 + numero2
+print("A soma é:", soma)
+
+# ----- Exercício 3 -----
+# Calculadora de Volume de uma Caixa Retangular
+comprimento = 12
+largura = 14
+altura = 20
+
+volume = comprimento * largura * altura
+
+print("O volume da caixa é:", volume, "cm³")
+
+# ----- Exercício 4 -----
+# Calculadora de Preço Total da Compra
+produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+preco_total = preco_unitario * quantidade
+
+print("Produto:", produto)
+print("Preço unitário: R$", preco_unitario)
+print("Quantidade:", quantidade)
+print("Preço total: R$", preco_total)
+
+# ----- Exercício 5 -----
+# Calculadora de Diferença
+A = int(input("Digite o valor de A: "))
+B = int(input("Digite o valor de B: "))
+C = int(input("Digite o valor de C: "))
+D = int(input("Digite o valor de D: "))
+
+diferenca = (A * B) - (C * D)
+
+print("DIFERENCA =", diferenca)
